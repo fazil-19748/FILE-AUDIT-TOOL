e8a1477bc4b413fa0ffa43a683d10ab05615a7a7
Mohammed Fazil <19748@geojittechnologies.com>
2025-07-03 12:16:48 +0530

    initial commit

---
+++ src/main/java/com/gtl/ImageAuditTool/ImageAuditToolApplication.java
@@ -0,0 +1,13 @@
+package com.gtl.FileAuditTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageAuditToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageAuditToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtl/ImageAuditTool/ImageAuditToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtl.FileAuditTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageAuditToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
